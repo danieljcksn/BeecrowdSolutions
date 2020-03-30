09908348b510b0ad1c8187ecb6d9b28741c8df2f
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-03-30 12:10:39 +0000

    Questão 1028 - Figurinhas

---
+++ Q1028.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int calculaMdc(int f1, int f2){
+	if(f1%f2 == 0)
+		return f2;
+	else
+		return calculaMdc(f2, f1 % f2);
+}
+
+int main(void){
+	int n, f1, f2, mdc;
+
+	scanf("%d", &n);
+
+	while(n>0){
+		scanf("%d %d", &f1, &f2);
+		mdc = calculaMdc(f1, f2);
+		printf("%d\n", mdc);
+		n--;
+	}
+	return 0;
+}
