a8709cafca72179b682c45ccca7f8e89737fd205
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-03-30 12:11:13 +0000

    Questão 2388 - Tacógrafo

---
+++ Q2388.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int n, i, T, V, total= 0;
+
+	scanf("%d", &n);
+	while(n>0){
+		scanf("%d %d", &T, &V);
+		total = total + T * V;
+		n--;
+	}
+
+	printf("%d\n", total);
+	return 0;
+}
