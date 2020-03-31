64d3767f24b4260f24e6fcb9cc6997f952ba74da
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-03-31 16:31:28 +0000

    Questão 1071 - Soma de Ímpares Consecutivos I

---
+++ Q1071.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+    int x, y, i, soma = 0;
+
+    scanf("%d %d", &x, &y);
+    if(y == x){
+        printf("0\n");
+        return 0;
+    }
+    if(y % 2 == 0 && x % 2 != 0){
+        for(i = y+1; i <= (x-2); i+=2)
+            soma += i;
+    }else{
+        if(y % 2 != 0 && x % 2 != 0){
+            for(i = y+2; i <= (x-2); i+=2)
+                soma += i;
+        }else{
+            if(y % 2 != 0 && x % 2 == 0){
+                for(i = y+2; i <= (x-1); i+=2)
+                    soma += i;
+            }else{
+                for(i = y+1; i <= (x-1); i+=2)
+                    soma += i;
+            }
+        }
+    }
+
+    printf("%d\n", soma);
+
+    return 0;
+}
