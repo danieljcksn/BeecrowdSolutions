746baa947d5d6fbee052011d44f95ab9c48bdf75
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-03-31 16:40:41 +0000

    Questão 1072 - Intervalo 2

---
+++ Q1072.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int n, i, in = 0, out = 0, x;
+
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &x);
+        if(x >= 10 && x <= 20)
+            in++;
+        else
+            out++;
+    }
+
+    printf("%d in\n", in);
+    printf("%d out\n", out);
+
+    return 0;
+}
