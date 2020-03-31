433d8dd05015a9de1da72315b1093a035880db1c
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-03-31 18:53:49 +0000

    Questão 1073 - Quadrado de Pares

---
+++ Q1073.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void){
+    int n, i;
+    unsigned long long int quadrado;
+
+    scanf("%d", &n);
+    for(i = 2; i <= n; i += 2){
+        quadrado = pow(i, 2);
+        printf("%d^2 = %llu\n", i, quadrado);
+    }
+
+
+    return 0;
+}
