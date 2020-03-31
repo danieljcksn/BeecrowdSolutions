2aad4cd04c6fc049f67e92540ce6b61059a16970
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-03-31 19:02:25 +0000

    Questão 1074 - Par ou Ímpar

---
+++ Q1074.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+    int n, x, i;
+
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &x);
+        if(x == 0)
+            printf("NULL\n");
+        else{
+            if(x % 2 == 0){
+                if(x > 0)
+                    printf("EVEN POSITIVE\n");
+                else
+                    printf("EVEN NEGATIVE\n");
+            }else{
+                if(x > 0)
+                    printf("ODD POSITIVE\n");
+                else
+                    printf("ODD NEGATIVE\n");
+            }
+        }
+    }
+
+
+    return 0;
+}
