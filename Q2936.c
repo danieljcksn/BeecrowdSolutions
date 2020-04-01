462609501a151d9f3f76f0f73dcb6b0ad4e2e6cd
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-01 18:39:21 +0000

    Questão 2936 - Quanta Mandioca?

---
+++ Q2936.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    int porcoes[5] = {300, 1500, 600, 1000, 150}, i, total=0, quantidade;
+
+    for(i = 0; i < 5; i++){
+        scanf("%d", &quantidade);
+        porcoes[i] = porcoes[i] * quantidade;
+    }
+
+    for(i = 0; i < 5; i++)
+        total += porcoes[i];
+
+    total += 225;
+    printf("%d\n", total);
+
+
+    return 0;
+}
