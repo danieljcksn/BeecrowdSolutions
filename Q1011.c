688b7d7790e5089b7399f15b573555328cc9c91e
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 12:02:31 +0000

    Questão 1011 - Esfera

---
+++ Q1011.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    double r;
+    double vol;
+    
+    scanf("%lf", &r);
+    vol = ((4.0/3)*3.14159*(pow(r,3)));
+    printf("VOLUME = %.3lf\n", vol);
+    return 0;
+}
