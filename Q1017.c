30dbe03284a97e9c849a4a93c6bc998110a1af73
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 12:06:02 +0000

    Questão 1017 - Gasto de Combustível

---
+++ Q1017.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ int main(void){
+
+ 	int tempo, vm, percorrido;
+ 	double litros;
+
+ 	scanf("%d %d", &tempo, &vm);
+
+ 	percorrido = (tempo * vm);
+	litros = (percorrido / 12.0);
+
+	printf("%.3lf\n", litros);
+
+ 	return 0;
+ }
