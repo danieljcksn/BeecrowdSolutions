da0446aed8ad63a99d6e96022cd4037539e0d6b6
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 12:02:00 +0000

    Questão 1010 - Cálculo Simples

---
+++ Q1010.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int CP1, NP1, CP2, NP2;
+    float VU1, VU2, TOTAL;
+    
+    scanf("%i %i %f", &CP1, &NP1, &VU1);
+    scanf("%i %i %f", &CP2, &NP2, &VU2);
+    TOTAL = VU1*NP1 + VU2*NP2;
+    printf("VALOR A PAGAR: R$ %.2f\n", TOTAL);
+    
+    return 0;
+}
