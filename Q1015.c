cbefd5245d0b47dec98c4684f3b466793dabcca0
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 12:04:54 +0000

    Questão 1015 - Distância Entre Dois Pontos

---
+++ Q1015.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	double x1, y1;
+	double x2, y2, d;
+
+	scanf("%lf %lf", &x1, &y1);
+	scanf("%lf %lf", &x2, &y2);
+
+	d = (sqrt(pow((x2-x1), 2)+pow((y2-y1), 2)));
+
+	printf("%.4lf\n", d);
+	return 0;
+}
