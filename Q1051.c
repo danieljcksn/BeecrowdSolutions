096a8d874aaa50a61687a18b8e05a2e04ab62e0d
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 11:21:58 +0000

    Questão 1051 - Imposto de Renda

---
+++ Q1051.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void){
+    float renda, imposto = 0;
+
+    scanf("%f", &renda);
+    if(renda >= 0 && renda <= 2000)
+        printf("Isento\n");
+    else{
+        if(renda <= 3000){
+            renda -= 2000;
+            imposto +=  0.08 * renda;
+            printf("R$ %.2f\n", imposto);
+        }   
+        else{
+            if(renda <= 4500){
+                renda -= 2000;
+                imposto += 0.08 * 1000;
+                renda -= 1000;
+                imposto += 0.18 * renda;
+                printf("R$ %.2f\n", imposto);
+            }else{
+                renda = renda - 2000;
+                imposto += 0.08 * 1000;
+                renda -= 1000;
+                imposto += 0.18 * 1500;
+                renda -= 1500;
+                imposto += 0.28 * renda;
+                printf("R$ %.2f\n", imposto);
+            }
+        }
+    }
+    return 0;
+}
