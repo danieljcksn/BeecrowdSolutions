7189c1620cadcff96c6a844fe0871f83ac3c1aa1
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 12:01:16 +0000

    Questão 1009 - Salário com Bônus

---
+++ Q1009.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char nome[15];
+    double sfixo;
+    double totalv;
+    double sfinal;
+    scanf("%s %lf %lf", &nome, &sfixo, &totalv);
+    sfinal = (sfixo + (0.15*totalv));
+    printf("TOTAL = R$ %.2lf\n", sfinal);
+    
+    return 0;
+}
