fd66e77e8dcff2e79797575377d0959063bc7072
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 11:56:27 +0000

    Questão 1006 - Média 2

---
+++ Q1006.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double A, B, C, MEDIA;
+    scanf("%lf %lf %lf", &A, &B, &C);
+    A = A*2;
+    B = B*3;
+    C = C*5;
+    MEDIA = (A+B+C) / 10;
+    printf("MEDIA = %.1lf\n", MEDIA);
+ 
+    return 0;
+}
