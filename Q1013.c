a13dc8edd6dfeefa573712ff51d571b7550d9772
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 12:03:40 +0000

    Questão 1013 - O Maior

---
+++ Q1013.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    int m, maior;
+
+    scanf("%i %i %i", &a, &b, &c);
+    m = ((a+b+abs(a-b))/2);
+    maior = ((m+c+abs(m-c))/2);
+    printf("%i eh o maior\n", maior);
+
+    return 0;
+}
