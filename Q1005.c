b45ce031ae969a6b48aac6e7c2b470cb9b23402b
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-04 11:55:34 +0000

    Questão 1005 - Média 1

---
+++ Q1005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double A, B, MEDIA;
+    
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    MEDIA = (((A*3.5)+(B*7.5))/11);
+    printf("MEDIA = %.5lf\n", MEDIA);
+    return 0;
+}
