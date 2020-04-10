419a743e2e4a110728303e2874805f502adfb415
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-10 16:55:56 +0000

    Questão 1068 - Balanço de Parênteses I

---
+++ Q1068.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    char expressao[1001];
+    int verificador, i, j;
+
+    
+    while(fgets(expressao, 1000, stdin) != NULL){
+        verificador = 0;
+        for(j = 0; j < strlen(expressao); j++){
+            if(expressao[j] == '(')
+                verificador++;
+            else{
+                if(expressao[j] == ')'){
+                    verificador--;
+                    if(verificador < 0)
+                        break;
+                }
+            }
+        }
+        if(verificador == 0)
+            printf("correct\n");
+        else
+            printf("incorrect\n");
+        expressao[0] = '\0';
+    }   
+
+    return 0;
+}
