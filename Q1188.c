a1818cd57fa39a0079ba1a6b8d8a01fab3ba672d
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-13 23:53:47 +0000

    Questão 1188 - Área Inferior

---
+++ Q1188.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+    int c, i, j;
+    double m[12][12], soma = 0.0, media, elementos = 0;
+    char o;
+
+    o = getc(stdin);
+    for(i = 0; i < 12; i++){
+        for(j = 0; j < 12; j++)
+            scanf("%lf", &m[i][j]);
+    }
+    for(i = 6; i < 12; i++){
+        for(j = 12 - i; j < i; j++){
+            soma += m[i][j];
+            elementos++;
+        }
+    }
+
+    if(o == 'S')
+        printf("%.1lf\n", soma);
+    else{    
+        media = soma / elementos;
+        printf("%.1lf\n", media);
+    }
+
+    return 0;
+}
