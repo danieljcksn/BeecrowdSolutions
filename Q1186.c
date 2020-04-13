edb54b8b486e40de242abe9cce0b1316a4f9a4e6
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-13 23:34:34 +0000

    Questão 1186 - Abaixo da Diagonal Secundária

---
+++ Q1186.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+    int c, i, j;
+    float m[12][12], soma = 0.0, media, elementos = 0;
+    char o;
+
+    o = getc(stdin);
+    for(i = 0; i < 12; i++){
+        for(j = 0; j < 12; j++)
+            scanf("%f", &m[i][j]);
+    }
+    for(i = 0; i < 12; i++){
+        for(j = 12-i; j < 12; j++){
+            soma += m[i][j];
+            elementos++;
+        }
+    }
+
+    if(o == 'S')
+        printf("%.1f\n", soma);
+    else{    
+        media = soma / elementos;
+        printf("%.1f\n", media);
+    }
+
+    return 0;
+}
