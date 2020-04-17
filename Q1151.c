e29b87115c789f4d1d11a965a9ce6fb63ad2de42
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-17 10:28:27 -0300

    Questão 1151 - Fibonacci Fácil

---
+++ Q1151.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int n, i;
+
+    scanf("%d", &n)
+    unsigned long int fatorial[n];
+    fatorial[0] = 0;
+    fatorial[1] = 1;
+    for(i = 2; i < n; i++)
+        fatorial[i] = fatorial[i-1] + fatorial[i-2];
+    
+    for(i = 0; i < n; i++){
+        if(i != n-1)
+            printf("%lu ", fatorial[i]);;
+        else
+            printf("%llu\n", fatorial[i]);
+    }
+
+
+    return 0;
+}
---
+++ Q1151.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, i;
+
+    cin >> n;
+    unsigned long int fatorial[n];
+    fatorial[0] = 0;
+    fatorial[1] = 1;
+    for(i = 2; i < n; i++)
+        fatorial[i] = fatorial[i-1] + fatorial[i-2];
+    
+    for(i = 0; i < n; i++){
+        if(i != n-1)
+            cout << fatorial[i] << " ";
+        else
+            cout << fatorial[i] << endl;
+    }
+
+
+    return 0;
+}
