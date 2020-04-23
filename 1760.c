a3c958678c088659c80a442d895dfb7dbc3a0b6b
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-04-23 17:37:27 -0300

    Questão 1760 - Floco de Neve
    Exercício resolvido com a utilização da fórmula fechada.

--- 1760.c
+++ 1760.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int main(void){
-    double l, areaTotal;
+    double l, areaTotal; 
     
     while(scanf("%lf", &l) != EOF){
         areaTotal = 2 * sqrt(3) * pow(l, 2) / 5;
