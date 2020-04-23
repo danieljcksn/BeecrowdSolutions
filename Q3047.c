df8c7c126eba3a6020aaae30bd24940320cd38c7
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-04-22 23:36:19 -0300

    Questão 3047 - A Idade de Dona Mônica

---
+++ Q3047.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int m, filhos[3], maior, i;
+    scanf("%d %d %d", &m, &filhos[0], &filhos[1]);
+ 
+    filhos[2] = m - (filhos[0] + filhos[1]);
+    maior = filhos[0];
+    for(i = 1; i < 3; i++)
+        if(maior < filhos[i])
+            maior = filhos[i];
+
+    printf("%d\n", maior);
+    return 0;
+}
---
+++ Q3047.cpp
@@ -0,0 +1,17 @@
+#include <iotream>
+#include <algorithms>
+
+using namespace std;
+
+int main(){
+    int m, filhos[3];
+    cin >> m;
+    cin >> filhos[0];
+    cin >> filhos[1];
+
+    filhos[2] = m - (filhos[0] + filhos[1]);
+    sort(filhos, filhos + 3);
+
+    cout << filhos[2] << endl;
+    return 0;
+}
