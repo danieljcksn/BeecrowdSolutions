240102f8bc45c4e301ef5e120bed122b8aaef8c1
daniielcavallcante <daniiel.cavallcante@gmail.com>
2020-05-06 16:53:50 -0300

    Questão 3055 - Nota esquecida

---
+++ 3055.c
@@ -0,0 +1,12 @@
+//Questão 3055 - Nota esquecida - URI Online Judge
+#include <stdio.h>
+
+int main(){
+    int a, b, m;
+    
+    scanf("%d %d", &a, &m);
+    b = 2 * m - a;
+    printf("%d\n", b);
+
+    return 0;
+}
---
+++ 3055.cpp
@@ -0,0 +1,13 @@
+//Questão 3055 - Nota esquecida - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, m;
+    cin >> a >> m;
+    b = 2 * m - a;
+    cout << b << endl;
+
+    return 0;
+}
