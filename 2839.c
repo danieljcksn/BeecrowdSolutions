6944df25b4c094c5116a6953fb595ae6c88fa76d
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-10 23:49:25 -0300

    Problema 2839 - As Meias de Rangel

---
+++ 2839.c
@@ -0,0 +1,11 @@
+//Problema 2839 - As Meias de Rangel - URI Online Judge
+#include <stdio.h>
+ 
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    printf("%d\n", n+1);
+    
+    return 0;
+}
---
+++ 2839.cpp
@@ -0,0 +1,13 @@
+//Problema 2839 - As Meias de Rangel - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+    cout << n+1 << endl;
+
+    return 0;
+}
