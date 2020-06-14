b8f1fb87972ea4b422535db33fcf61fe2271bdd2
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-13 22:40:22 -0300

    Problema 1097 - Sequencia IJ 3

---
+++ 1097.cpp
@@ -0,0 +1,16 @@
+//Problema 1097 - Sequencia IJ 3 - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i, j, k;
+    
+    for(i = 1; i <= 9; i++){
+        j = i + 6;
+        for(k = 0; k < 3; k++){
+            cout << 
+    }
+
+    return 0;
+}
