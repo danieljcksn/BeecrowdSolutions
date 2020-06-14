bbad3d8363bf9bef12a74dbaefddf7063c9aa942
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-13 23:46:21 -0300

    Problema 2058 - Triângulos e Polígonos Regulares

---
+++ 2058.cpp
@@ -0,0 +1,14 @@
+//Problema 2058 - Triângulos e Polígonos Regulares - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    cout << n - 2 << endl;
+
+    return 0;
+}
