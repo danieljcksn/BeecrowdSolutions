a1549b3f9702f4e244bb60889e7de712ef6a6d68
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-13 23:15:35 -0300

    Problema 1589 - Bob Conduite

---
+++ 1589.cpp
@@ -0,0 +1,16 @@
+//Problema 1589 - Bob Conduite - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t, r1, r2;
+
+    cin >> t;
+    while(t--){
+        cin >> r1 >> r2;
+        cout << r1 + r2 << '\n';
+    }
+
+    return 0;
+}
