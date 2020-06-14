99f5b89f32df79e7247ddec94c7b4dfabf968df1
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-13 23:30:46 -0300

    Problema 1933 - Tri-du

---
+++ 1933.cpp
@@ -0,0 +1,17 @@
+//Problema 1933 - Tri-du - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cin >> a >> b;
+
+    if(a > b)
+        cout << a << '\n';
+    else 
+        cout << b << '\n';
+
+    return 0;
+}
