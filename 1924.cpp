e9c3833fbaa7a219dc2621742762e43f1b2450c2
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-13 23:23:20 -0300

    Problema 1924 - Vitória e a Indecisão

---
+++ 1924.cpp
@@ -0,0 +1,17 @@
+//Problema 1924 - Vitória e a Indecisão - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nome;
+    int n;
+
+    cin >> n;
+    while(n--)
+        cin >> nome;
+
+    cout << "Ciencia da Computacao\n";
+
+    return 0;
+}
