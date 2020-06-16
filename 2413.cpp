0f3f150429dccd701053ba968396619e486fd11a
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-16 20:42:12 -0300

    Problema 2413 - Busca na Internet

---
+++ 2413.cpp
@@ -0,0 +1,14 @@
+//Problema 2413 - Busca na Internet - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+
+    cin >> t;
+
+    cout << 4*t << endl;
+
+    return 0;
+}
