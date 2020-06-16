877aa0b9046c44cee2df9642de5a1563e127ef93
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-16 20:22:46 -0300

    Problema 2374 - Pneu

---
+++ 2374.cpp
@@ -0,0 +1,14 @@
+//Problema 2374 - Pneu - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+
+    cin >> n >> m;
+
+    cout << n - m << endl;
+
+    return 0;
+}
