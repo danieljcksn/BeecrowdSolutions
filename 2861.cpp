09d3b44c6932325b5654289273d9379396f0063f
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-16 21:07:57 -0300

    Problema 2861 - A Saída

---
+++ 2861.cpp
@@ -0,0 +1,17 @@
+//Problema 2861 - A Saída - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int c;
+
+    cin >> c;
+
+    while(c--){
+        cin.ignore();
+        cout << "gzuz\n";
+    }
+
+    return 0;
+}
