615fd349e460cddf99f79da54cda5607f79a4050
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-16 22:00:10 -0300

    Problema 2581 - I am Toorg!

---
+++ 2581.cpp
@@ -0,0 +1,17 @@
+//Problema 2581 - I am Toorg! - URI Online Judge
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    while(n--){
+        cin.ignore();
+        cout << "I am Toorg!\n";
+    }
+
+    return 0;
+}
