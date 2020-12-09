8cf8e011bff2a13e40eeec423a35987623bc2a83
Daniel <daniiel.cavallcante@gmail.com>
2020-12-09 16:59:17 -0300

    Template C++

--- template.cpp
+++ template.cpp
@@ -1,10 +1,15 @@
-//Problema  -  - URI Online Judge
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#define FOR(i, a, b) for(int i = a; i <= b; ++i)
+#define RFOR(i, a, b) for(int i = a; i >= b; --i)
+
+typedef unsigned long long int ull;
+
 int main(){
-    
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
 
     return 0;
 }
