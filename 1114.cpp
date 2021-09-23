2e3528a0b867ea8e0aaea996d5228b2aa67e1d4d
Daniel <daniiel.cavallcante@gmail.com>
2021-09-23 13:39:18 -0300

    1114 - Senha Fixa

---
+++ 1114.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    while(true){
+        int pass;
+
+        cin >> pass;
+
+        if(pass != 2002)
+            cout << "Senha Invalida\n";
+        else
+            break;
+    }
+
+    cout << "Acesso Permitido\n";
+
+    return 0;
+}
