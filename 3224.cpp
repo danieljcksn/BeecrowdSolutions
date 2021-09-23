8f134d150177967efa7535356be4170a7a3b491b
Daniel <daniiel.cavallcante@gmail.com>
2021-09-23 13:46:52 -0300

    3224 - Aaah!

---
+++ 3224.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string a, b;
+
+    cin >> a >> b;
+
+    (a.size() >= b.size()) ? cout << "go\n" : cout << "no\n";
+
+    return 0;
+}
